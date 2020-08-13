117aab38eb17da55d0eeecd165618aff3dabaf9a
M K <mk@example.com>
2020-08-13 19:53:02 +0000

    Change var.location to var.region

--- storageacc.tf
+++ storageacc.tf
@@ -15,7 +15,7 @@ variable "Storage_Account_Name" {
 resource "azurerm_storage_account" "sa" {
     name = var.Storage_Account_Name
     resource_group_name = var.ResourceGroup
-    location = var.location
+    location = var.region
     account_tier = "Standard"
     account_replication_type = "GRS"
 
